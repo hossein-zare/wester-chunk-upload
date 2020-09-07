2e28bde0d852feb4913ae691de3c2a19bb29a652
Hossein Zare <zarehossein.hz1997@gmail.com>
2020-09-07 15:57:18 +0430

    Add response() method.

--- src/Exceptions/MainException.php
+++ src/Exceptions/MainException.php
@@ -2,7 +2,18 @@
 
 namespace Wester\ChunkUpload\Exceptions;
 
+use Wester\ChunkUpload\Response;
+
 class MainException extends \Exception
 {
-    //
+    /**
+     * Create a new instance of the response class.
+     * 
+     * @param  null|int  $status
+     * @return \Wester\ChunkUpload\Response
+     */
+    public function response($status = null)
+    {
+        return (new Response())->status($status);
+    }
 }
