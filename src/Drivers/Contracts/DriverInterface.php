f97cc390bd9efd951b182f543cbbb680c9e222a6
Hossein Zare <zarehossein.hz1997@gmail.com>
2020-09-13 18:21:28 +0430

    Delete common interface definitions.

--- src/Drivers/Contracts/DriverInterface.php
+++ src/Drivers/Contracts/DriverInterface.php
@@ -4,18 +4,12 @@ namespace Wester\ChunkUpload\Drivers\Contracts;
 
 interface DriverInterface
 {
-    public function delete(): void;
-    public function store(string $tmpName): void;
-    public function move(): void;
-    public function increase(): void;
-    public function createTempFileName(int $part = null): string;
-    public function createRandomString(): string;
-    public function createFileName(): string;
-    public function getTempFilePath(int $part = null): string;
-    public function getFilePath(): string;
-    public function getFileName(): string;
-    public function getFullFileName(): string;
-    public function getFileExtension();
+    public function open();
+    public function close();
+    public function store($fileName);
+    public function delete();
+    public function move();
+    public function increase();
     public function prevExists();
-    public function exists(): bool;
+    public function exists();
 }
